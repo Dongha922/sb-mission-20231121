61ada100a362552b8464d1eebc5ba6ed170ccc13
dongha922 <kdh4575@naver.com>
2023-11-21 16:11:04 +0900

    세팅중

---
+++ src/main/java/com/ll/sbmission20231121/SbMission20231121Application.java
@@ -0,0 +1,13 @@
+package com.ll.sbmission20231121;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMission20231121Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMission20231121Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/sbmission20231121/SbMission20231121ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbmission20231121;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMission20231121ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
