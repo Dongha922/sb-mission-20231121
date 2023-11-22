1e49715dc19353f917ff6fa5ccdaf57c37ba0269
동하이 <kdh4575@naver.com>
2023-11-22 16:17:35 +0900

    E/1 (#2)
    * h2데이터베이스연결
    
    * JPA엔티티
    
    * 서버문제 해결

---
+++ src/main/java/com/ll/sbmission20231121/Answer.java
@@ -0,0 +1,29 @@
+package com.ll.sbmission20231121;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    //답변의 고유 번호
+    @Column(columnDefinition  = "TEXT")
+    private String content;
+    //답변 내용
+    private LocalDateTime createDate;
+    //답변 작성 일시
+    @ManyToOne
+    private Question question;
+    //어떤 질문에 대한 답변 인지 알기 위한 질문 속성
+
+
+
+}
