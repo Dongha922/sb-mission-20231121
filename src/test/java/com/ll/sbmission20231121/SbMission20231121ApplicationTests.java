ac284a4069be9823870ad6bef123cf8726458614
Dongha922 <kdh4575@naver.com>
2023-12-04 15:31:55 +0900

    ëfindByID 사용

--- src/test/java/com/ll/sbmission20231121/SbMission20231121ApplicationTests.java
+++ src/test/java/com/ll/sbmission20231121/SbMission20231121ApplicationTests.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import java.time.LocalDateTime;
-
+import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -16,29 +16,16 @@ class SbMission20231121ApplicationTests {
 	//객체 주입 setter 대신 사용
 	private QuestionRepository questionRepository;
 
-	/*@Test
-	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject ("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-
-		this.questionRepository.save(q1);//첫번째 질문 저장
-
-
-		Question q2 = new Question();
-		q2.setSubject ("스프링부트모델질문입니다");
-		q2.setContent("id는 자동 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);//두 번째 질문 저장
-	}*/
+//findByID 는 리턴 타입 X, Optional 임
+	//Optional 은 null 을 유연하게 처리 하는 클래스
+	//isPresent로 null 이 아님 get으로 실제 Qustion 객체 값을 얻음.
 	@Test
 	void testJpa(){
-		List<Question> all =this.questionRepository.findAll();//저장된거 다 찾아와
-		assertEquals(2,all.size());//전체가 2일텐데 맞나?
-
-		Question q= all.get(0);//젤 처음꺼 가져와
-		assertEquals("sbb가 무엇인가요?", q.getSubject());//젤 처음꺼 제목 이거일텐데 맞나?
+		Optional<Question> oq = this.questionRepository.findById(1);
+		if(oq.isPresent()){
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
 	}
 
 }
