a035284ebd36167e35fa5d903674d3be6403c14a
Dongha922 <kdh4575@naver.com>
2023-12-04 17:52:22 +0900

    데이터수정하기

--- src/test/java/com/ll/sbmission20231121/SbMission20231121ApplicationTests.java
+++ src/test/java/com/ll/sbmission20231121/SbMission20231121ApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.awt.desktop.QuitStrategy;
 import java.util.List;
@@ -29,9 +30,13 @@ class SbMission20231121ApplicationTests {
 //findByID 는 리턴 타입 X, Optional 임
 	//Optional 은 null 을 유연 하게 처리 하는 클래스
 	//isPresent 로 null 이 아님 get 으로 실제 Question 객체 값을 얻음.
+	//```void testJpa(){
+	//		Optional<Question> oq = this.questionRepository.findById(1);
+	//		if(oq.isPresent()){
+	//			Question q= oq.get();
+	//			assertEquals("sbb가 무엇인가요?", q.getSubject());	}
 
-//findBYSubject
-	//QuestionRepository 인터 페이스 수정
+//findBySubject
 	//메소드 선언 만으로 구현은 하지 않고 실행 가능
 	//JpaRepository 를 상속한 QuestionRepository 객체가 생성 될때만 가능.
 	//```Question findBySubject(String subject);```
@@ -51,11 +56,18 @@ class SbMission20231121ApplicationTests {
 	//			List<Question qList = this.questionRepository.findBySubjectLike("sbb%");
 	//			Question q = qList.get(0);
 	//			assertEquals("sbb가 무엇인가요?", q.getSubject());
+
+//데이터 수정하기
+	//assertTrue(값) 은 값이 true인지 테스트
+	//this.questionRepository.save(q)처럼 리포지터리의 save 메소드 사용.
 	@Test
 	void testJpa(){
-		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-		Question q = qList.get(0);
-		assertEquals("sbb가 무엇인가요?", q.getSubject());
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 제목");
+		this.questionRepository.save(q);
+
 
 	}
 
